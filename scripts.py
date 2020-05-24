7854c6b225fcabb26eaee4a8f666106a43dd37e1
edoarad <edoarad92@gmail.com>
2020-05-24 11:19:59 +0300

    Fixed encoding

--- scripts.py
+++ scripts.py
@@ -1,12 +1,12 @@
 import xml.etree.ElementTree as et
-import csv
+import unicodecsv as csv
 from pathlib import Path
 
 
 def make_csv_from_ART(ART_corpus_dir_path, csv_file_path):
     directories = [Path(ART_corpus_dir_path + '\\ann' + str(i)) for i in range(1,10)]
-    with open(csv_file_path, 'w', newline='', encoding='UTF-8') as csv_file:
-        csv_writer = csv.writer(csv_file)
+    with open(csv_file_path, 'wb') as csv_file:
+        csv_writer = csv.writer(csv_file, dialect="excel", encoding='UTF-8')
         csv_writer.writerow(["Paper", "Annotation", "Content"])
         for d in directories:
             for xfile in d.iterdir():
