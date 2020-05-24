c66d42cf990cf837c69a686587a4b539e8cd944f
edoarad <edoarad92@gmail.com>
2020-05-24 11:02:53 +0300

    fixed parsing with inner tags

--- scripts.py
+++ scripts.py
@@ -7,6 +7,7 @@ def make_csv_from_ART(ART_corpus_dir_path, csv_file_path):
     directories = [Path(ART_corpus_dir_path + '\\ann' + str(i)) for i in range(1,10)]
     with open(csv_file_path, 'w', newline='', encoding='UTF-8') as csv_file:
         csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(["Paper", "Annotation", "Content"])
         for d in directories:
             for xfile in d.iterdir():
                 name = (str(xfile.name))[:-4]
@@ -14,5 +15,5 @@ def make_csv_from_ART(ART_corpus_dir_path, csv_file_path):
                 root = tree.getroot()
                 for annot in root.iter('annotationART'):
                     typ = annot.attrib['type']
-                    sen = annot.text
+                    sen = "".join(annot.itertext()) # Captures all sub-tags
                     csv_writer.writerow([name, typ, sen])
